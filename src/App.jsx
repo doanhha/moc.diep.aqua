8298e0efde1dd8ee149dbd4436d34142d4c28f35
doanhha <dhavan4444@gmail.com>
2025-11-13 01:51:56 +0700

    up-date-3

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import 'bootstrap/dist/css/bootstrap.css'
 import './assets/css/style.css'
 import Header from './Components/Header.jsx'
+import Producttop from './Layout/Producttop.jsx'
 
 const App = () => {
   return (
     <>
     <Header/>
+    <Producttop/>
     </>
   )
 }
--- src/Layout/Producttop.jsx
+++ src/Layout/Producttop.jsx
@@ -1,88 +1,127 @@
-import React from 'react'
+import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Pagination } from "swiper/modules";
+
+// import CSS của Swiper
+import "swiper/css";
+// import "swiper/css/navigation";
+import "swiper/css/pagination";
 
 const Producttop = () => {
-    return (
-        <>
-            <div className="swiper container featured-products mt-0">
-                <div className="block-title">
-                    <div className="head mt-4 ml-4">
-                        <span className="icon">
-                            <img src="//bizweb.dktcdn.net/100/533/545/themes/971699/assets/save-icon.png?1758000527031"
-                                alt="Giờ vàng giảm giá!" className="img-flash-sale" />
-                        </span>
-                        <div className="heading-title span-vip">
-                            <h2><a href="san-pham-ban-chay" title="Giờ vàng giảm giá!">Giờ vàng giảm giá!</a></h2>
-                        </div>
-                    </div>
-                    <div className="timer">
-                        <div className="time" data-countdown="countdown" data-date="2-13-2026-12-00-00" />
-                    </div>
-                </div>
-                <div className="card-wrapper swiper mt-4">
-                    <ul className="card-list swiper-wrapper pl-0">
-                        <li className="card-item swiper-slide">
-                            <a href="#" className="card-link">
-                                <img src="./assets/img/product-1.webp" alt="Image" className="card-image" />
-                                <h2 className="card-title">Lorem ipsum dolor sit amet.</h2>
-                                <span className="span-vip">1.000.000đ</span>
-                                <p>Còn hàng</p>
-                                <p className="badge">Developer</p>
-                                {/* <button class="card-button ti-arrow-right"></button> */}
-                            </a>
-                        </li>
-                        {/* 2 */}
-                        <li className="card-item swiper-slide">
-                            <a href="#" className="card-link">
-                                <img src="./assets/img/product-1.webp" alt="Image" className="card-image" />
-                                <h2 className="card-title">Lorem ipsum dolor sit amet.</h2>
-                                <span className="span-vip">1.000.000đ</span>
-                                <p>Còn hàng</p>
-                                <p className="badge">Developer</p>
-                                {/* <button class="card-button ti-arrow-right"></button> */}
-                            </a>
-                        </li>
-                        {/* 3 */}
-                        <li className="card-item swiper-slide">
-                            <a href="#" className="card-link">
-                                <img src="./assets/img/product-1.webp" alt="Image" className="card-image" />
-                                <h2 className="card-title">Lorem ipsum dolor sit amet.</h2>
-                                <span className="span-vip">1.000.000đ</span>
-                                <p>Còn hàng</p>
-                                <p className="badge">Developer</p>
-                                {/* <button class="card-button ti-arrow-right"></button> */}
-                            </a>
-                        </li>
-                        {/* 4 */}
-                        <li className="card-item swiper-slide">
-                            <a href="#" className="card-link">
-                                <img src="./assets/img/product-1.webp" alt="Image" className="card-image" />
-                                <h2 className="card-title">Lorem ipsum dolor sit amet.</h2>
-                                <span className="span-vip">1.000.000đ</span>
-                                <p>Còn hàng</p>
-                                <p className="badge">Developer</p>
-                                {/* <button class="card-button ti-arrow-right"></button> */}
-                            </a>
-                        </li>
-                        {/* 5 */}
-                        <li className="card-item swiper-slide">
-                            <a href="#" className="card-link">
-                                <img src="./assets/img/product-1.webp" alt="Image" className="card-image" />
-                                <h2 className="card-title">Lorem ipsum dolor sit amet.</h2>
-                                <span className="span-vip">1.000.000đ</span>
-                                <p>Còn hàng</p>
-                                <p className="badge">Developer</p>
-                                {/* <button class="card-button ti-arrow-right"></button> */}
-                            </a>
-                        </li>
-                    </ul>
-                    <div className="swiper-pagination" />
-                    {/* If we need navigation buttons */}
-                    <div className="swiper-slide-button swiper-button-prev" />
-                    <div className="swiper-slide-button swiper-button-next" />
-                </div>
-            </div>
-        </>
-    )
-}
+  const products = [
+    {
+      id: 1,
+      name: "Lorem ipsum dolor sit amet.",
+      price: "1.000.000đ",
+      status: "Còn hàng",
+      badge: "Developer",
+      image: "./assets/img/product-1.webp",
+    },
+    {
+      id: 2,
+      name: "Lorem ipsum dolor sit amet.",
+      price: "1.000.000đ",
+      status: "Còn hàng",
+      badge: "Developer",
+      image: "./assets/img/product-1.webp",
+    },
+    {
+      id: 3,
+      name: "Lorem ipsum dolor sit amet.",
+      price: "1.000.000đ",
+      status: "Còn hàng",
+      badge: "Developer",
+      image: "./assets/img/product-1.webp",
+    },
+    {
+      id: 4,
+      name: "Lorem ipsum dolor sit amet.",
+      price: "1.000.000đ",
+      status: "Còn hàng",
+      badge: "Developer",
+      image: "./assets/img/product-1.webp",
+    },
+    {
+      id: 5,
+      name: "Lorem ipsum dolor sit amet.",
+      price: "1.000.000đ",
+      status: "Còn hàng",
+      badge: "Developer",
+      image: "./assets/img/product-1.webp",
+    },
+    {
+      id: 5,
+      name: "Lorem ipsum dolor sit amet.",
+      price: "1.000.000đ",
+      status: "Còn hàng",
+      badge: "Developer",
+      image: "./assets/img/product-1.webp",
+    },
+  ];
 
-export default Producttop
+  return (
+    <>
+      <div className="container featured-products mt-0">
+        {/* Header / Tiêu đề */}
+        <div className="block-title">
+          <div className="head mt-4 ml-4">
+            <span className="icon">
+              <img
+                src="//bizweb.dktcdn.net/100/533/545/themes/971699/assets/save-icon.png?1758000527031"
+                alt="Giờ vàng giảm giá!"
+                className="img-flash-sale"
+              />
+            </span>
+            <div className="heading-title span-vip">
+              <h2>
+                <a href="san-pham-ban-chay" title="Giờ vàng giảm giá!">
+                  Giờ vàng giảm giá!
+                </a>
+              </h2>
+            </div>
+          </div>
+          <div className="timer">
+            <div
+              className="time"
+              data-countdown="countdown"
+              data-date="2-13-2026-12-00-00"
+            />
+          </div>
+        </div>
+
+        {/* Swiper */}
+        <Swiper
+          modules={[Navigation, Pagination]}
+          spaceBetween={16}
+          slidesPerView={4}
+          pagination={{ clickable: true }}
+          // Giữ class để dùng lại css cũ nếu có
+          className="card-wrapper mt-4 card-list"
+          breakpoints={{
+            0: { slidesPerView: 1 },
+            640: { slidesPerView: 2 },
+            1024: { slidesPerView: 4 },
+          }}
+        >
+          {products.map((item) => (
+            <SwiperSlide key={item.id} className="card-item">
+              <a href="#" className="card-link">
+                <img
+                  src={item.image}
+                  alt={item.name}
+                  className="card-image"
+                />
+                <h2 className="card-title">{item.name}</h2>
+                <span className="span-vip">{item.price}</span>
+                <p>{item.status}</p>
+                <p className="badge">{item.badge}</p>
+              </a>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </div>
+    </>
+  );
+};
+
+export default Producttop;
