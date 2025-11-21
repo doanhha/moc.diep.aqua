6fede7f7e22674112a007ba6408ea807c5078dc9
doanhha <dhavan4444@gmail.com>
2025-11-21 21:53:27 +0700

    tính năng: cải tiến thành phần Danh sách sản phẩm với bố cục và kiểu dáng mới cho biểu ngữ và bán hàng

--- src/Components/ProductsList.jsx
+++ src/Components/ProductsList.jsx
@@ -1,10 +1,25 @@
 import React from 'react'
 import Header from './Header'
+import Topbar from '../Layout/Topbar'
+import Menu from '../Layout/Menu'
+import SearchLogo from '../Layout/SearchLogo'
 
 const ProductsList = () => {
+    const imagesBanner = [
+        { id: 1, name: 'Banner', image: "./assets/img/product-image-1.webp" }
+    ]
+    const pruductSale = [
+        {id: 1, Name: "Tôm Càng Xanh Sống", origin: "Xuất xứ: Việt Nam", price: "1.000.000"},
+        {id: 1, Name: "Tôm Càng Xanh Sống", origin: "Xuất xứ: Việt Nam", price: "1.000.000"},
+        {id: 1, Name: "Tôm Càng Xanh Sống", origin: "Xuất xứ: Việt Nam", price: "1.000.000"},
+        {id: 1, Name: "Tôm Càng Xanh Sống", origin: "Xuất xứ: Việt Nam", price: "1.000.000"},
+        {id: 1, Name: "Tôm Càng Xanh Sống", origin: "Xuất xứ: Việt Nam", price: "1.000.000"},
+    ]
     return (
         <>
-        <Header />
+            <Topbar />
+            <SearchLogo />
+            <Menu />
             <div style={{ backgroundColor: "#EBF5FF" }} className="bg-home">
                 <div className="container">
                     <div className="row">
@@ -19,6 +34,13 @@ const ProductsList = () => {
                     </div>
                 </div>
             </div>
+            {imagesBanner.map((item =>
+                <div className="container">
+                    <img src={item.image} alt={item.name} className="img-banner" />
+                    <h2 className="sale">Hàng giá tốt</h2>
+                </div>
+            ))}
+
         </>
     )
 }
