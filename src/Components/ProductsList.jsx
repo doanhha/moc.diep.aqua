353da1b690ccffb3662814e0e239cedd5361fda6
doanhha <dhavan4444@gmail.com>
2025-11-22 17:42:47 +0700

    Cấu trúc mã được cải tiến để cải thiện khả năng đọc và bảo trì

--- src/Components/ProductsList.jsx
+++ src/Components/ProductsList.jsx
@@ -1,19 +1,19 @@
 import React from 'react'
-import Header from './Header'
 import Topbar from '../Layout/Topbar'
 import Menu from '../Layout/Menu'
 import SearchLogo from '../Layout/SearchLogo'
+import { Swiper, SwiperSlide } from 'swiper/react'
 
 const ProductsList = () => {
     const imagesBanner = [
         { id: 1, name: 'Banner', image: "./assets/img/product-image-1.webp" }
     ]
-    const pruductSale = [
-        {id: 1, Name: "Tôm Càng Xanh Sống", origin: "Xuất xứ: Việt Nam", price: "1.000.000"},
-        {id: 1, Name: "Tôm Càng Xanh Sống", origin: "Xuất xứ: Việt Nam", price: "1.000.000"},
-        {id: 1, Name: "Tôm Càng Xanh Sống", origin: "Xuất xứ: Việt Nam", price: "1.000.000"},
-        {id: 1, Name: "Tôm Càng Xanh Sống", origin: "Xuất xứ: Việt Nam", price: "1.000.000"},
-        {id: 1, Name: "Tôm Càng Xanh Sống", origin: "Xuất xứ: Việt Nam", price: "1.000.000"},
+    const productSale = [
+        { id: 1, name: "Tôm Càng Xanh Sống", origin: "Xuất xứ: Việt Nam", price: "1.000.000", image: "./assets/img/product-1.webp" },
+        { id: 1, name: "Tôm Càng Xanh Sống", origin: "Xuất xứ: Việt Nam", price: "1.000.000", image: "./assets/img/product-2.webp" },
+        { id: 1, name: "Tôm Càng Xanh Sống", origin: "Xuất xứ: Việt Nam", price: "1.000.000", image: "./assets/img/product-1.webp" },
+        { id: 1, name: "Tôm Càng Xanh Sống", origin: "Xuất xứ: Việt Nam", price: "1.000.000", image: "./assets/img/product-1.webp" },
+        { id: 1, name: "Tôm Càng Xanh Sống", origin: "Xuất xứ: Việt Nam", price: "1.000.000", image: "./assets/img/product-1.webp" },
     ]
     return (
         <>
@@ -26,8 +26,8 @@ const ProductsList = () => {
                         <div className="col-12 col-md-8 info-left">
                             <nav aria-label="breadcrumb">
                                 <ol className="breadcrumb">
-                                    <li className="breadcrumb-item"><a style={{ textDecoration: "none", color: "black" }} href="#">Trang chủ</a></li>
-                                    <li className="breadcrumb-item active" aria-current="page">Giới thiệu</li>
+                                    <li className="breadcrumb-item"><a style={{ textDecoration: "none", color: "black", }} href="#">Trang chủ</a></li>
+                                    <li className="breadcrumb-item active span-vip" aria-current="page">Tất cả sản phẩm</li>
                                 </ol>
                             </nav>
                         </div>
@@ -38,6 +38,30 @@ const ProductsList = () => {
                 <div className="container">
                     <img src={item.image} alt={item.name} className="img-banner" />
                     <h2 className="sale">Hàng giá tốt</h2>
+                    {/* list products */}
+                    <Swiper
+                        spaceBetween={20}
+                        slidesPerView={3}
+                    >
+                        {productSale.map((productItem) => (
+                            <SwiperSlide key={productItem.id}>
+                                <div className="product-sale-item">
+                                    <img
+                                        src={productItem.image}
+                                        alt=""
+                                        className="product-img-sale"
+                                    />
+
+                                    <div className="product-info">
+                                        <h4 className="product-name">{productItem.name}</h4>
+                                        <p style={{fontSize: 16, color: "#595959ff"}} className="product-origin">{productItem.origin}</p>
+                                        <p style={{color: "red"}} className="product-price">{productItem.price} VND</p>
+                                    </div>
+                                </div>
+                            </SwiperSlide>
+                        ))}
+
+                    </Swiper>
                 </div>
             ))}
 
